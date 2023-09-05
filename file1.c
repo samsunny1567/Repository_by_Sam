12d0a1701fe5b56a356e2669009cc178db2f5534
ANORA\samsunny <samsunny@anorasolutions.com>
2023-09-05 17:44:47 +0530

    power is added

--- file1.c
+++ file1.c
@@ -5,12 +5,13 @@ int addition(int x,int y);
 int subtraction(int x,int y);
 int multiplication(int x, int y);
 int division(int x,int y);
+int power(int,int);
 
 int main()
 {
     int x,y,select;
     int result=0;
-    printf("enter 1 for addition , 2 for subtraction 3 for multiplication 4 for division:  ");
+    printf("enter 1 for addition , 2 for subtraction 3 for multiplication 4 for division\n 5 for power:  ");
     scanf("%d",&select);
     printf("enter first number: ");
     scanf("%d",&x);
@@ -35,6 +36,10 @@ int main()
          result=operation(x,y,division);
          printf("Result = %d",result);  
          break;
+         case 5:
+         result=operation(x,y,power);
+         printf("Result = %u",result);
+
     }
 
     return 0;
@@ -60,4 +65,13 @@ int multiplication(int x,int y)
 int division(int x,int y)
 {
     return x/y;
+}
+int power(int x,int y)
+{
+    int result=1;
+    for(int i=0;i<y;i++)
+    {
+        result=x*result;
+    }
+    return result;
 }
